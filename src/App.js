d563c7213750fad0e38c9aec4fb3a955ac8d12d4
emanoelpequeno <emanoelpequeno5@gmail.com>
2024-10-23 15:38:55 -0300

    modified:   src/App.css 	modified:   src/App.js 	new file:   src/Componentes/Header/estilo.css 	new file:   src/Componentes/Header/index.js 	modified:   src/Componentes/OpcoesHeader/estilo.css

--- src/App.js
+++ src/App.js
@@ -1,17 +1,11 @@
 import './App.css';
-import Logo from './componentes/Logo'
-import IconesHeader from './componentes/IconesHeader';
-import OpcoesHeader from './componentes/OpcoesHeader';
+import Header from './componentes/Header';
 
 
 function App() {
   return (
     <div className='App'>
-      <header className='App-header'>
-        <Logo></Logo>
-        <OpcoesHeader></OpcoesHeader>
-        <IconesHeader></IconesHeader>
-      </header>
+      <Header />
     </div>
   );
 }
