1488569b99ab2bd9a957043b71e122c98282c10e
emanoelpequeno <emanoelpequeno5@gmail.com>
2024-10-23 15:08:24 -0300

    modified:   src/App.js 	new file:   src/Componentes/IconesHeader/estilo.css 	new file:   src/Componentes/IconesHeader/index.js 	modified:   src/Componentes/Logo/estilo.css 	new file:   src/Componentes/OpcoesHeader/estilo.css 	new file:   src/Componentes/OpcoesHeader/index.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,15 @@
 import './App.css';
-import Logo from './Componentes/Logo'
-import perfil from './imagens/perfil.svg'
-import sacola from './imagens/sacola.svg'
+import Logo from './componentes/Logo'
+import IconesHeader from './componentes/IconesHeader';
 
-const textoOpcoes = ['Categorias', 'Favoritos', 'Minha Estante']
-const icones = [perfil, sacola]
 
 function App() {
   return (
     <div className='App'>
       <header className='App-header'>
         <Logo></Logo>
-        <ul className='opcoes'>
-          {textoOpcoes.map( (texto) => (
-            <li className='opcao'><p>{texto}</p></li>
-            ) ) }
-        </ul>
-        <ul className='icones'>
-          {icones.map( (icone) => (
-            <li className='icone'><img src={icone}></img></li>
-            ) ) }
-        </ul>
+        <OpcoesHeader></OpcoesHeader>
+        <IconesHeader></IconesHeader>
       </header>
     </div>
   );
