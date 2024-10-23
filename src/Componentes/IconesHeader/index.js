a8c49181b8b3c145830092547aa2eabf4a5f39c9
emanoelpequeno <emanoelpequeno5@gmail.com>
2024-10-23 16:57:03 -0300

    adiciona o styled components

--- src/Componentes/IconesHeader/index.js
+++ src/Componentes/IconesHeader/index.js
@@ -1,16 +1,27 @@
-import './estilo.css'
 import perfil from '../../imagens/perfil.svg'
 import sacola from '../../imagens/sacola.svg'
+import styled from 'styled-components'
+
+const Icones = styled.ul`
+    align-items: center;
+    display: flex;
+}
+`
+const Icone = styled.li`
+    width: 30px;
+    padding: 0 10px;
+}
+`
 
 const icones = [perfil, sacola]
 
 function IconesHeader() {
     return (
-        <ul className='icones'>
+        <Icones>
           {icones.map( (icone) => (
-            <li className='icone'><img src={icone}></img></li>
+            <Icone><img src={icone}></img></Icone>
             ) ) }
-        </ul>
+        </Icones>
     )
 }
 
