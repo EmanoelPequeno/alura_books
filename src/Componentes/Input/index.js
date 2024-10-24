afc9b4568ca45329ad3ad425404faca367fbc5e3
emanoelpequeno <emanoelpequeno5@gmail.com>
2024-10-24 17:53:56 -0300

    Implementa tratamentos na pesquisa

--- src/Componentes/Input/index.js
+++ src/Componentes/Input/index.js
@@ -6,7 +6,7 @@ const Input = styled.input`
     border: 1px solid #FFF;
     padding: 20px 30px;
     border-radius: 50px;
-    width: 400px;
+    width: 300px;
     color: #FFF;
     font-size: 16px;
     margin-bottom: 10px;
--- src/Componentes/Pesquisa/index.js
+++ src/Componentes/Pesquisa/index.js
@@ -1,19 +1,21 @@
 import Input from '../Input';
 import styled from 'styled-components'
 import React, { useState } from 'react';
+import { livros } from './dadosPequisa'
 
 const PesquisaContainer = styled.section`
     color: #FFF;
     text-align: center;
     padding: 85px 0;
+    width: 100vw;
     height: 270px;
-    width: 100%;
 `
 const Titulo = styled.h2`
     color: #FFF;
     font-size: 36px;
     text-align: center;
     width: 100%;
+    margin: 10px 0px;
 `
 const Subtitulo = styled.h3`
     font-size: 16px;
@@ -29,18 +31,43 @@ const Button = styled.button`
     cursor: pointer;
     padding: 14px 24px;
     margin-left: 15px;
+    
+    &:disabled {
+        cursor: not-allowed;
+    }
 `
+const LivroContainer = styled.div`
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+`
+const MensagemErro = styled.p`
+    color: #FF6347; /* Tom de vermelho para erro */
+    margin-top: 20px;
+    font-size: 14px;
+`;
 
 function Pesquisa() {
     
+    const [livrosPesquisados, setLivrosPesquisados] = useState([]);
     const [textoDigitado, setTextoDigitado] = useState('');
-    const [inputValue, setInputValue] = useState('');
-    
+    const [mensagemErro, setMensagemErro] = useState('');
+
     const handleInput = (e) => {
-        setInputValue(e.target.value);
+        setTextoDigitado(e.target.value.trim());
+        setMensagemErro('');
     };
+
     const handleBuscar = () => {
-        setTextoDigitado(inputValue);
+        const resultadoPesquisa = livros.filter((livro) => livro.nome.toLowerCase().includes(textoDigitado.toLowerCase()));
+        
+        if (resultadoPesquisa.length === 0) {
+            setMensagemErro('Poxa, não encontramos nenhum livro com esse nome. Tente buscar outro livro.');
+            setLivrosPesquisados([]);
+        } else {
+            setLivrosPesquisados(resultadoPesquisa);
+            setMensagemErro('');
+        }
     };
 
     const [isFocused, setIsFocused] = useState(false);
@@ -53,18 +80,36 @@ function Pesquisa() {
         setIsFocused(false);
     };
 
+    const handleKeyPress = (e) => {
+        if (e.key === 'Enter' && textoDigitado.trim()) {
+          handleBuscar();
+        }
+    };
+
     return (
         <PesquisaContainer>
             <Titulo>Já sabe por onde começar?</Titulo>
-            <Subtitulo>Encontre seu livro em nossa estante.</Subtitulo>
+            <Subtitulo>Encontre seu livro.</Subtitulo>
             <Input 
                 placeholder= {isFocused ? '' : 'Buscar livro...'} 
                 onFocus={handleOnFocus}
                 onBlur={handleBlur}
-                onChange={handleInput} 
+                onChange={handleInput}
+                onKeyUp={handleKeyPress} 
             />
-            <Button onClick={handleBuscar}>Buscar</Button>
-            <p>{textoDigitado}</p>
+            <Button 
+                onClick={handleBuscar}
+                disabled={!textoDigitado.trim()
+            }>Buscar</Button>
+            <LivroContainer>
+                {livrosPesquisados.map((livro) => (
+                    <div>
+                        <img src={livro.src} alt={livro.nome} />
+                        <li key={livro.id}>{livro.nome}</li>
+                    </div>
+                ))}
+            </LivroContainer>
+            {mensagemErro && <MensagemErro>{mensagemErro}</MensagemErro>}
         </PesquisaContainer>        
     )
 }
